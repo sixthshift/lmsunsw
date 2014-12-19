b2a824182cf4e539c0ca236115eb26142a298e7c
Jason Huang <sixth.shifts@gmail.com>
2014-12-19 21:54:40 +1100

    first commit, added user registration and related pages

---
+++ lmsunsw/app/forms.py
@@ -0,0 +1,32 @@
+"""
+Definition of forms.
+"""
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
+
+from app.models import *
+
+class RegisterUserForm(UserCreationForm):
+
+    #email = forms.EmailField(required=True)
+    #first_name = forms.CharField(max_length=30)
+    #last_name = forms.CharField(max_length=30)
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2", "first_name", "last_name", "email")
+
+    def save(self, commit=True):
+        new_user=User.objects.create_user(self.cleaned_data['username'], self.cleaned_data['email'], self.cleaned_data['password1'])
+        new_user.first_name = self.cleaned_data['first_name']
+        new_user.last_name = self.cleaned_data['last_name']
+        #user = super(UserCreationForm, self).save(commit=False)
+        #user.email = self.cleaned_data["email"]
+        #user.first_name = self.cleaned_data["first_name"]
+        #user.last_name = self.cleaned_data["last_name"]
+        if commit:
+            new_user.save()
+        return new_user
