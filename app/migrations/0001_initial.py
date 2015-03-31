3cb7b97c4b50f10ea7f45b37cf0f220fc71f5732
Jason Huang <sixth.shifts@gmail.com>
2015-03-31 18:32:14 +1100

    filled out models.py and added migrations

---
+++ app/migrations/0001_initial.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Lecture',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('lecture_name', models.CharField(unique=True, max_length=30)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Quiz',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('question', models.TextField()),
+                ('visible', models.BooleanField(default=False)),
+                ('Lecture', models.ForeignKey(to='app.Lecture')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='QuizChoice',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('choice', models.TextField()),
+                ('correct', models.BooleanField(default=False)),
+                ('Quiz', models.ForeignKey(to='app.Quiz')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='QuizChoiceSelected',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('quiz_choice', models.ForeignKey(to='app.QuizChoice')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user', models.OneToOneField(related_name='user_profile', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- app/models.py
+++ app/models.py
@@ -1,3 +1,56 @@
-from django.db import models
+"""
+Definition of models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from fluent_contents.models import PlaceholderField
+from fluent_contents.models import Placeholder
+import string
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='user_profile')
+    profile_picture = PlaceholderField("profile_picture")
+    # like a toString
+    def __unicode__(self):
+        return unicode(self.user)
+
+class Lecture(models.Model):
+    lecture_name = models.CharField(max_length=30, unique=True)
+    collab_doc = PlaceholderField("lecture_docs")
+
+    @property
+    def lecture_docs_used(self):
+        #returns whether the PlaceholderField is being used or not
+        return bool(len(Placeholder.objects.filter(slot='lecture_docs', parent_id=self.id)))
+
+    @property
+    def get_slug_field(self):
+        return self.lecture_name.replace(' ','_')
+
+    @property
+    def get_absolute_url(self):
+        return self.id + "/" + get_slug_field()
+
+    def __unicode__(self):
+        return unicode(self.lecture_name)
+
+class Quiz(models.Model):
+    question = models.TextField()
+    visible = models.BooleanField(default=False)
+    Lecture = models.ForeignKey(Lecture)
+
+    def __unicode__(self):
+        return unicode(self.Lecture.lecture_name + " " + self.question)
+
+
+class QuizChoice(models.Model):
+    choice = models.TextField()
+    Quiz = models.ForeignKey(Quiz)
+    correct = models.BooleanField(default=False)
+    #times_chosen = models.PositiveSmallIntegerField()
+    
+
+class QuizChoiceSelected(models.Model):
+    user = models.ForeignKey(User)
+    quiz_choice = models.ForeignKey(QuizChoice)
