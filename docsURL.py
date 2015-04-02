fdee4172ab77cb852ecfe3a86bab2188f968ad5f
Jason Huang <sixth.shifts@gmail.com>
2015-04-02 16:18:42 +1100

    added list of hardcoded google docs files since cannot programmatically create them on aws

---
+++ docsURL.py
@@ -0,0 +1,13 @@
+# List of available google docs URL 
+# The google documents are located at:
+# jasonhuang.2015@gmail.com
+# password: passpassword
+glist = [
+'https://docs.google.com/document/d/1QTvDx4NaaGugjwWB-TgceNAZsE9Re1wEyY4wY0Bd1cw/edit?usp=sharing',
+'https://docs.google.com/document/d/1igFNUwj3bbyp-mPX9GcAQfUwG5AuXm71X9jKCtn52A0/edit?usp=sharing',
+'https://docs.google.com/document/d/12JoPsP3xGAS4qR22KIiDH5__rXXHgl_4HoCGnF8PZJE/edit?usp=sharing',
+'https://docs.google.com/document/d/1bpPLGDO_go3tFMd9Jsuyl3wm8o0MhuoRmDKMkqohNzE/edit?usp=sharing',
+'https://docs.google.com/document/d/1QjbM0grrY-RulEKTZKzDRddx2DcWfj9VdmZPNyvsgew/edit?usp=sharing',
+'https://docs.google.com/document/d/1md2CYh3MJc2t7R04txr1s0-UtAgQ1sHJFfn6Tb_U_Uk/edit?usp=sharing',
+'https://docs.google.com/document/d/1dVS5qwO9wt3Dbxotds6XeVeCXCAfJf0BGZ2jcaRYUac/edit?usp=sharing',
+]
