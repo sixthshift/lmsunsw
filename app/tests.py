2616f4b60350558ec3cfce94a2fa7bd310d9b502
Sheryl Shi <sheshy_2004@hotmail.com>
2015-04-20 18:44:57 +1000

    just need to increment views for new posts

--- app/tests.py
+++ app/tests.py
@@ -8,8 +8,8 @@ from populate import Rand
 class User_Case(TestCase):
 
 	def setUp(self):
-		User.objects.create(username="AAA", first_name="A", last_name="A", email="A@test.com", password="A", is_superuser=True)
 		User.objects.create(username="BBB", first_name="B", last_name="B", email="B@test.com", password="B", is_superuser=True)
+		User.objects.create(username="AAA", first_name="A", last_name="A", email="A@test.com", password="A", is_superuser=True)
 		User.objects.create(username="CCC", first_name="C", last_name="C", email="C@test.com", password="C", is_superuser=False)
 		User.objects.create(username="DDD", first_name="D", last_name="D", email="D@test.com", password="D", is_superuser=False)
 		User.objects.create(username="EEE", first_name="E", last_name="E", email="E@test.com", password="E", is_superuser=True)
@@ -248,3 +248,88 @@ class Quiz_Usage(TestCase):
 		self.assertEquals(Quiz.objects.first().quiz_type, QuizType.MULTIMCQ)
 
 
+Class Forum_Post_New(TestCase):
+
+	def setUp(self):
+		Thread.objects.create(title="Apple", content="Types of Fruit", Creator=1, views=0, anonymous=False)
+		Thread.objects.create(title="Cars", content="Types of Cars", Creator=3, views=0, anonymous=True)
+		Thread.objects.create(title="Class", content="Types of Class", Creator=4, views=0, anonymous=False)
+		Thread.objects.create(title="Colour", content="Types of Colour", Creator=8, views=0, anonymous=True)
+		Thread.objects.create(title="Books", content="Types of Books", Creator=11, views=0, anonymous=False)
+
+
+	def test_correct_attr(self):
+		self.assertEquals(Thread.objects.get(id=1).title, "Apple")		
+		self.assertEquals(Thread.objects.get(id=2).title, "Cars")		
+		self.assertEquals(Thread.objects.get(id=3).title, "Class")		
+		self.assertEquals(Thread.objects.get(id=4).title, "Colour")		
+		self.assertEquals(Thread.objects.get(id=5).title, "Books")
+
+		self.assertEquals(Thread.objects.get(id=1).content, "Types of Fruit")
+		self.assertEquals(Thread.objects.get(id=2).content, "Types of Cars")		
+		self.assertEquals(Thread.objects.get(id=3).content, "Types of Class")
+		self.assertEquals(Thread.objects.get(id=4).content, "Types of Colour")
+		self.assertEquals(Thread.objects.get(id=5).content, "Types of Books")
+
+		self.assertEquals(Thread.objects.get(id=1).Creator, 1)
+		self.assertEquals(Thread.objects.get(id=2).Creator, 3)
+		self.assertEquals(Thread.objects.get(id=3).Creator, 4)
+		self.assertEquals(Thread.objects.get(id=4).Creator, 8)
+		self.assertEquals(Thread.objects.get(id=5).Creator, 11)
+
+		for i in xrange(1,5):
+			self.assertEquals(Thread.objects.get(id=i).views, 0)
+
+		self.assertEquals(Thread.objects.get(id=1).anonymous, False)		
+		self.assertEquals(Thread.objects.get(id=2).anonymous, True)
+		self.assertEquals(Thread.objects.get(id=3).anonymous, False)
+		self.assertEquals(Thread.objects.get(id=4).anonymous, True)
+		self.assertEquals(Thread.objects.get(id=5).anonymous, False)
+
+Class Forum_Thread_New(TestCase):
+	
+	def setUp(self):
+		Post.objects.create(Thread=1, content="Apple", Creator=4, anonymous=False)
+		Post.objects.create(Thread=1, content="banana", Creator=2, anonymous=False)
+		Post.objects.create(Thread=1, content="orange", Creator=8, anonymous=False)
+		Post.objects.create(Thread=2, content="Volvo is a car brand", Creator=9, anonymous=True)
+		Post.objects.create(Thread=2, content="The question was types of Cars", Creator=11, anonymous=True)
+		Post.objects.create(Thread=4, content="Blue", Creator=1, anonymous=True)
+		Post.objects.create(Thread=5, content="Harry Potter", Creator=2, anonymous=True)
+		Post.objects.create(Thread=5, content="Oceans", Creator=3, anonymous=True)
+		Post.objects.create(Thread=5, content="Owls", Creator=10, anonymous=True)
+
+	def test_correct_attr(self):
+
+		for i in xrange(1,3):
+			self.assertEquals(Post.objects.get(id=i).Thread, 1)
+		self.assertEquals(Post.objects.get(id=4).Thread, 2)
+		self.assertEquals(Post.objects.get(id=5).Thread, 2)
+		self.assertEquals(Post.objects.get(id=6).Thread, 4)
+		for x in xrange(7,9):
+			self.assertEquals(Post.objects.get(id=x).Thread, 5)
+
+		self.assertEquals(Post.objects.get(id=1).content, "Apple")
+		self.assertEquals(Post.objects.get(id=2).content, "banana")
+		self.assertEquals(Post.objects.get(id=3).content, "orange")
+		self.assertEquals(Post.objects.get(id=4).content, "Volvo is a car brand")
+		self.assertEquals(Post.objects.get(id=5).content, "The questionwas types of Cars")
+		self.assertEquals(Post.objects.get(id=6).content, "Blue")
+		self.assertEquals(Post.objects.get(id=7).content, "Harry Potter")
+		self.assertEquals(Post.objects.get(id=8).content, "Oceans")
+		self.assertEquals(Post.objects.get(id=9).content, "Owls")
+
+		self.assertEquals(Post.objects.get(id=1).Creator, 4)
+		self.assertEquals(Post.objects.get(id=2).Creator, 2)
+		self.assertEquals(Post.objects.get(id=3).Creator, 8)
+		self.assertEquals(Post.objects.get(id=4).Creator, 9)
+		self.assertEquals(Post.objects.get(id=5).Creator, 11)
+		self.assertEquals(Post.objects.get(id=6).Creator, 1)
+		self.assertEquals(Post.objects.get(id=7).Creator, 2)
+		self.assertEquals(Post.objects.get(id=8).Creator, 3)
+		self.assertEquals(Post.objects.get(id=9).Creator, 10)
+
+		for n in xrange (1,3):
+			self.assertEquals(Post.objects.get(id=n).anonymous, False)
+		for m in xrange(4,9):
+			self.assertEquals(Post.objects.get(id=m).anonymous, True)
